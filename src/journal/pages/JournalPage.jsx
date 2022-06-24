06ad4d6ea445e363ba0ddbfa3a2da534a1f0411e
Ivan <devdnia@gmail.com>
2022-06-24 21:18:45 +0200

    boton agregar notes

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,6 @@
 
+import { useDispatch, useSelector } from 'react-redux';
+import { startNewNote } from '../../store/journal/thunks';
 import { NoteView, NothingSelectedView } from '../views';
 import { JournalLayout } from '../layout/JournalLayout';
 import { IconButton } from '@mui/material';
@@ -7,29 +9,46 @@ import { AddOutlined } from '@mui/icons-material';
 
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+  const {isSaving, active} = useSelector( state => state.journal)
+
+  const onClickNewNotes = () => {
+
+    dispatch(startNewNote());
+
+  }
+
+
+  
   return (
 
     <JournalLayout>
 
-      <NothingSelectedView />
-
-      {/*<NoteView />*/}
+    {
+      (!!active)
+      ? <NoteView />
+      : <NothingSelectedView />
+    }
 
       <IconButton
+        disabled={isSaving}
+        onClick={onClickNewNotes}
         size='large'
         sx={{
           color: 'white',
           backgroundColor: 'error.main',
-          ':hover':{
+          ':hover': {
             backgroundColor: 'error.main',
-            opacity:0.9
+            opacity: 0.9
           },
           position: 'fixed',
           right: 50,
           bottom: 50,
         }}
       >
-        <AddOutlined sx={{ fontSize: 30}}/>
+
+        <AddOutlined sx={{ fontSize: 30 }} />
       </IconButton>
     </JournalLayout>
   )
---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,56 @@
+
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+    name: 'journal',
+    initialState: {
+        isSaving: false,
+        messageSaved: '',
+        notes: [],
+        active: null,
+
+        /** Ejemplo de nota
+         * active: [
+         *  id: 'adaddadf',
+         *  title: '',
+         *  body: '',
+         *  date: 1223431,
+         *  imageUrls: [], https://photo1.jpg, ...
+         * ]
+         */
+
+    },
+    reducers: {
+        savingNewNote: (state) => {
+            state.isSaving = true;
+        },
+        addNewEmptyNote: (state, action) => {
+            state.notes.push(action.payload);
+            state.isSaving = false;
+        },
+        setActiveNote: (state, action) => {
+            state.active = action.payload;
+        },
+        setNotes: (state, action) => {
+        },
+        setSaving: (state) => {
+        },
+        updateNote: (state, action) => {
+        },
+        deleteNoteById: (state, action) => {
+        },
+    }
+
+});
+
+
+// Action creators are generated for each case reducer function
+export const {
+    savingNewNote,
+    addNewEmptyNote,
+    setActiveNote,
+    setNotes,
+    setSaving,
+    updateNote,
+    deleteNoteById,
+} = journalSlice.actions;
